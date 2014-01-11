fb9bd46c559d49b022a320ac0163a8b9f7ca1049
Flanord <Flanord@gmail.com>
2014-01-11 03:28:13 +0000

    KArdex Cajas

--- CityTruck.WebSite/App/Model/Cajas/Kardex.js
+++ CityTruck.WebSite/App/Model/Cajas/Kardex.js
@@ -1,19 +1,15 @@
-﻿Ext.define('App.Model.Ventas.DetallesVenta', {
+﻿Ext.define('App.Model.Cajas.Kardex', {
     extend: 'Ext.data.Model',
     fields: [
             { type: "date", name: "FECHA", dateFormat: "d/m/Y", convert: Funciones.Fecha },
-            { type: "int", name: "ID_POS" },
-            { type: "int", name: "ID_POS_TURNO" },
-            { type: "float", name: "ENT_LITTER" },
-            { type: "float", name: "SAL_LITTER" },
-            { type: "float", name: "TOTAL" },
-            { type: "string", name: "PRODUCTO" }
+            { type: "int", name: "ID_CAJA" },
+            { type: "int", name: "ID_KARDEX" },
+            { type: "int", name: "ID_OPERACION" },
+            { type: "string", name: "OPERACION" },
+            { type: "string", name: "DETALLE" },
+            { type: "float", name: "INGRESO" },
+            { type: "float", name: "EGRESO" },
+            { type: "float", name: "SALDO" },
+            { type: "int", name: "ID_USUARIO" }
         ]
 });
-
-//PRODUCTO = x.SG_POS.CODIGO + " - " + x.SG_POS.SG_COMBUSTIBLES.NOMBRE,
-//                ID_POS = x.ID_POS,
-//                ID_POS_TURNO = x.ID_POS_TURNO,
-//                ENT_LITTER = x.ENT_LITTER,
-//                SAL_LITTER = x.SAL_LITTER,
-//                TOTAL = x.ENT_LITTER - x.SAL_LITTER
--- CityTruck.WebSite/App/View/Cajas/Principal.js
+++ CityTruck.WebSite/App/View/Cajas/Principal.js
@@ -1,4 +1,5 @@
-﻿Ext.define("App.View.Cajas.Principal", {
+﻿var id_caja = 0;
+Ext.define("App.View.Cajas.Principal", {
     extend: "App.Config.Abstract.PanelPrincipal",
     controlador: 'Ventas',
     accionGrabar: 'GrarbarCaja',
@@ -18,6 +19,7 @@
             imagenes: false,
             opcion: 'GridCajas'
         });
+
         me.items = [me.grid
         ];
         me.toolbar = Funciones.CrearMenuBar();
@@ -28,14 +30,18 @@
         Funciones.CrearMenu('btn_Eliminar', 'Eliminar', Constantes.ICONO_BAJA, me.EventosCaja, me.toolbar, this);
         //        Funciones.CrearMenu('btn_PlanillaRelevamiento', 'Planilla para Relevamiento', Constantes.ICONO_VER, me.EventosPlanilla, me.toolbar, this);
         me.grid.addDocked(me.toolbar, 1);
-        //        me.grid.on('cellclick', me.CargarDatos, this);
+
+        me.grid.on('itemclick', function (view, record, item, index, e) {
+            console.log(record.get('ID_CAJA'));
+            id_caja = record.get('ID_CAJA');
+        }, this);
 
     },
     EventosCaja: function (btn) {
         var me = this;
         if (btn.getItemId() == "btn_CrearCaja") {
             if (me.winCrearCaja == null) {
-                me.winCrearCaja = Ext.create("App.Config.Abstract.Window", {botones : true});
+                me.winCrearCaja = Ext.create("App.Config.Abstract.Window", { botones: true });
                 me.formCrearCaja = Ext.create("App.View.Cajas.FormCaja", {
                     columns: 1,
                     title: 'Registro de Cajas ',
@@ -47,13 +53,77 @@
             } else {
                 me.winCrearCaja.show();
             }
+        } else if (btn.getItemId() == 'btn_Kardex') {
+            me.CrearVentanaKardex();
         }
         else {
             Ext.Msg.alert("Aviso", "No Existe el botton");
         }
+
     },
     GuardarCajas: function () {
         var me = this;
         Funciones.AjaxRequestWin('Cajas', 'GuardarCaja', me.winCrearCaja, me.formCrearCaja, me.grid, 'Esta Seguro de Guardar la Caja?', null, me.winCrearCaja);
+    }, CrearVentanaKardex: function () {
+        var me = this;
+        var buttonGroup = [{
+            xtype: 'button',
+            text: 'Nuevo',
+            iconCls: 'add',
+            minHeight: 27,
+            minWidth: 80,
+            handler: function () {
+                alert('Evento Nuevo');
+            }
+
+        }, {
+            xtype: 'button',
+            text: 'Detalle',
+            iconCls: 'report',
+            minHeight: 27,
+            minWidth: 80,
+            handler: function () {
+                alert('Evento Detalle');
+            }
+
+        }, {
+            xtype: 'button',
+            text: 'Imprimir',
+            iconCls: 'printer',
+            minHeight: 27,
+            minWidth: 80,
+            handler: function () {
+                alert('Evento Imprimir');
+            }
+
+        }, {
+            xtype: 'button',
+            text: 'Cerrar',
+            iconCls: 'cross',
+            minHeight: 27,
+            minWidth: 80,
+            handler: function () {
+                this.up('window').hide();
+            }
+
+        }];
+
+        if (me.winKardexCaja == null) {
+            me.winKardexCaja = Ext.create("App.Config.Abstract.Window", { botones: false, buttons: buttonGroup });
+            me.gridKardexCaja = Ext.create("App.View.Cajas.GridKardexCaja", {
+                region: 'center',
+                width: 700,
+                height: 350,
+                imagenes: false,
+                opcion: 'GridKardexCaja',
+                id_caja: id_caja
+            });
+            me.winKardexCaja.add(me.gridKardexCaja);
+            me.winKardexCaja.show();
+        } else {
+            me.gridKardexCaja.store.setExtraParams({ ID_CAJA: id_caja });
+            me.gridKardexCaja.store.load();
+            me.winKardexCaja.show();
+        }
     }
 });
